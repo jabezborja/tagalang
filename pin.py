01af79c41afe480725ef4c934c020494a1c43dac
jabezborja <jabez.natsu@gmail.com>
2021-10-01 16:41:03 +0800

    Add: Process time

--- pin.py
+++ pin.py
@@ -1,12 +1,15 @@
 from compiler.parser import Parser
 from compiler.lexer import Lexer
+import time
 import sys
 
 def main():
     arg = sys.argv[1]
     
     with open(arg) as fs:
+        processtime_in_ms = time.time() * 1000
         result = Parser(Lexer(fs.read()).generate_tokens())
+        print(f"\nPROCESS TIME: {time.time() * 1000 - processtime_in_ms}ms")
         
 if __name__ == '__main__':
     main()
